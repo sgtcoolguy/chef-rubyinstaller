a2f309c06147548ce248ac4bcee7aba0a007ac7b
Evan Machnic <emachnic@uship.com>
2016-11-30 11:53:14 -0600

    Updated version dependencies

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description 'Installs/Configures RubyInstaller'
 long_description 'Installs/Configures RubyInstaller'
 issues_url 'https://github.com/emachnic/chef-rubyinstaller/issues'
 source_url 'https://github.com/emachnic/chef-rubyinstaller'
-version '0.1.3'
+version '0.1.4'
 
 supports 'windows'
 
-depends 'chef-sugar', '~> 3.3.0'
-depends 'windows',    '~> 1.44.1'
+depends 'chef-sugar', '~> 3.4'
+depends 'windows',    '~> 2.1'
--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -17,8 +17,3 @@ end
 describe file('C:\\DevKit-x64\config.yml') do
   its(:content) { should match(%r{- C:\/Ruby23-x64}) }
 end
-
-describe command('gem install json --platform=ruby') do
-  its(:stdout) { should match(/Building native extensions/) }
-  its(:stdout) { should match(/Successfully installed json/) }
-end
